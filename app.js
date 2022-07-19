158ced2ff008c19d1cf21bcdc70fc1a6c00c8242
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-07-19 08:50:37 -0500

    first conditional dynamic html working

--- app.js
+++ app.js
@@ -1,3 +1,53 @@
+const fs = require("fs");
+const generatePage = require("./src/page-template");
+
+// const mockData = {
+//   // ! just mock data for easier testing!
+//   name: "Lernantino",
+//   github: "lernantino",
+//   confirmAbout: true,
+//   about:
+//     "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et.",
+//   projects: [
+//     {
+//       name: "Run Buddy",
+//       description:
+//         "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.",
+//       languages: ["HTML", "CSS"],
+//       link: "https://github.com/lernantino/run-buddy",
+//       feature: true,
+//       confirmAddProject: true,
+//     },
+//     {
+//       name: "Taskinator",
+//       description:
+//         "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.",
+//       languages: ["JavaScript", "HTML", "CSS"],
+//       link: "https://github.com/lernantino/taskinator",
+//       feature: true,
+//       confirmAddProject: true,
+//     },
+//     {
+//       name: "Taskmaster Pro",
+//       description:
+//         "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque. Nulla eget fringilla nulla. Integer gravida magna mi, id efficitur metus tempus et. Nam fringilla elit dapibus pellentesque cursus.",
+//       languages: ["JavaScript", "jQuery", "CSS", "HTML", "Bootstrap"],
+//       link: "https://github.com/lernantino/taskmaster-pro",
+//       feature: false,
+//       confirmAddProject: true,
+//     },
+//     {
+//       name: "Robot Gladiators",
+//       description:
+//         "Duis consectetur nunc nunc. Morbi finibus non sapien nec pharetra. Fusce nec dignissim orci, ac interdum ipsum. Morbi mattis justo sed commodo pellentesque.",
+//       languages: ["JavaScript"],
+//       link: "https://github.com/lernantino/robot-gladiators",
+//       feature: false,
+//       confirmAddProject: false,
+//     },
+//   ],
+// };
+
 const inquirer = require("inquirer");
 // import inquirer from "inquirer";
 
@@ -140,15 +190,14 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+    // const pageHTML = generatePage(mockData); //to test more easily...
+
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw new Error(err);
+
+      console.log(
+        "Page created! Check out index.html in this directory to see it!"
+      );
+    });
   });
-// const fs = require("fs");
-// const generatePage = require("./src/page-template");
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile("./index.html", pageHTML, (err) => {
-//   if (err) throw err;
-
-//   console.log("Portfolio complete! Check out index.html to see the output!");
-// });
